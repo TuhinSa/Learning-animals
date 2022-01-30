fa3972cd09b0ec4fbf1a4041171d232932b9c552
TuhinSa <92089255+TuhinSa@users.noreply.github.com>
2022-01-30 18:21:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+function lion_audio() {
+    document.getElementById("lionaudio").play();
+}
+function hippo_audio() {
+    document.getElementById("hippoaudio").play();   
+}
+function rhino_audio() {
+    document.getElementById("rhinnoaudio").play();   
+}
+function donkey_audio() {
+    document.getElementById("donkeyaudio").play();   
+}
+function elephant_audio() {
+    document.getElementById("elephantaudio").play();   
+}
+function bird_audio() {
+    document.getElementById("birdaudio").play();   
+}
+function dog_audio() {
+    document.getElementById("dogaudio").play();   
+}
+function cat_audio() {
+    document.getElementById("cataudio").play();   
+}
+function sheep_audio() {
+    document.getElementById("sheepaudio").play();   
+}
+function rooster_audio() {
+    document.getElementById("roosteraudio").play();   
+}
